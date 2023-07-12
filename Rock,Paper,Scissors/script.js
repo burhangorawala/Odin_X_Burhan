6989a9e827cc75792f51a731e362e9aabd19c09b
burhangorawala <gorawalab@gmail.com>
2023-07-12 13:45:26 +0530

    R P S COMPLETED

--- Rock,Paper,Scissors/script.js
+++ Rock,Paper,Scissors/script.js
@@ -1,14 +1,16 @@
-function playround (userChoice){
-    var choices = [rock,paper,scissor];
-    var computerChoice = choices[Math.floor(Math.random() * choices.length)];
-    var result = '';
-    if(userChoice == computerChoice) result = "Its a Tei";
-    else if(
-        (userChoice === "rock" && computerChoice === "scissors") ||
-        (userChoice === "paper" && computerChoice === "rock") ||
-        (userChoice === "scissors" && computerChoice === "paper")
-    ) result = "You Win";
-    else result = "You Lose";
+function playround(userChoice) {
+  var choices = ["rock", "paper", "scissor"];
+  var computerChoice = choices[Math.floor(Math.random() * choices.length)];
+  var result = "";
+  if (userChoice == computerChoice) result = "Its a Tei";
+  else if (
+    (userChoice === "rock" && computerChoice === "scissors") ||
+    (userChoice === "paper" && computerChoice === "rock") ||
+    (userChoice === "scissors" && computerChoice === "paper")
+  )
+    result = "You Win";
+  else result = "You Lose";
+  document.getElementById("computerChoice").innerText = computerChoice;
 
-    return 
+  document.getElementById("result").innerText = result;
 }
