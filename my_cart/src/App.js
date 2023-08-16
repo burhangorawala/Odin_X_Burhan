e520dfb1397706483ddcf4bc4bed11d28708f967
burhangorawala <gorawalab@gmail.com>
2023-08-16 13:50:29 +0530

    meh

--- my_cart/src/App.js
+++ my_cart/src/App.js
@@ -1,11 +1,27 @@
 import logo from "./logo.svg";
 import "./App.css";
-import navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
+import ProductsList from './components/ProductList.js'
 
 function App() {
+  const product = [
+    {
+      price: 99999,
+      name: "iPhone 14",
+      quantity: 0,
+    },
+    {
+      price: 9999,
+      name: "Mi Note 10",
+      quantity: 0,
+    }
+  ]
   return(
     <>
-    <navbar/>
+  <Navbar/>
+  <ProductsList product={product}/>
+  {/* <Footer/> */}
+
   </>
   )
 };
---
+++ my_cart/src/components/ProductList.js
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+export default function ProductList({product}) {
+    console.log({product})
+  return (
+    <div>
+
+    </div>
+  )
+}
+
